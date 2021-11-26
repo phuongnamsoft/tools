b69257cb9ae60105a9029b3baa99575619bbee94
phuongnamsoft <phuongnam@phuongnam.org>
2021-11-26 12:26:53 +0700

    add 74 full

--- vst-downgrade.sh
+++ vst-downgrade.sh
@@ -4,4 +4,8 @@ rm -rf /usr/local/vesta/ioncube/ioncube.sh
 
 yum downgrade vesta-0.9.8-26 vesta-php-0.9.8-26 vesta-nginx-0.9.8-26 vesta-ioncube-0.9.8-26 -y
 
+bash vestacp/vst-crack.sh
+
 service vesta restart
+
+bash vestacp/vst-crack.sh
